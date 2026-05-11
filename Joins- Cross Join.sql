d304a5260a7baa888af77a288029117daabea4f3
VaruneshT <tvarunesharasu@gmail.com>
2026-05-11 23:50:21 +0530

    Update Joins- Cross Join.sql

---
+++ Joins- Cross Join.sql
@@ -0,0 +1,41 @@
+create database crossjoin;
+use crossjoin;
+CREATE TABLE IF NOT EXISTS emp (
+    empno INT PRIMARY KEY,
+    ename VARCHAR(20),
+    job VARCHAR(20),
+    mgr INT,
+    hiredate DATE,
+    sal DECIMAL(7, 2),
+    comm DECIMAL(7, 2),
+    deptno INT
+);
+INSERT INTO emp VALUES (7369, 'SMITH', 'CLERK', 7902, '1980-12-17', 800, NULL, 20);
+INSERT INTO emp VALUES (7499, 'ALLEN', 'SALESMAN', 7698, '1981-02-20', 1600, 300, 30);
+INSERT INTO emp VALUES (7521, 'WARD', 'SALESMAN', 7698, '1981-02-22', 1250, 500, 30);
+INSERT INTO emp VALUES (7566, 'JONES', 'MANAGER', 7839, '1981-04-02', 2975, NULL, 20);
+INSERT INTO emp VALUES (7654, 'MARTIN', 'SALESMAN', 7698, '1981-09-28', 1250, 1400, 30);
+INSERT INTO emp VALUES (7698, 'BLAKE', 'MANAGER', 7839, '1981-05-01', 2850, NULL, 30);
+INSERT INTO emp VALUES (7782, 'CLARK', 'MANAGER', 7839, '1981-06-09', 2450, NULL, 10);
+INSERT INTO emp VALUES (7788, 'SCOTT', 'ANALYST', 7566, '1982-12-09', 3000, NULL, 20);
+INSERT INTO emp VALUES (7839, 'KING', 'PRESIDENT', NULL, '1981-11-17', 5000, NULL, 10);
+INSERT INTO emp VALUES (7844, 'TURNER', 'SALESMAN', 7698, '1981-09-08', 1500, 0, 30);
+INSERT INTO emp VALUES (7876, 'ADAMS', 'CLERK', 7788, '1983-01-12', 1100, NULL, 20);
+INSERT INTO emp VALUES (7900, 'JAMES', 'CLERK', 7698, '1981-12-03', 950, NULL, 30);
+INSERT INTO emp VALUES (7902, 'FORD', 'ANALYST', 7566, '1981-12-03', 3000, NULL, 20);
+INSERT INTO emp VALUES (7934, 'MILLER', 'CLERK', 7782, '1982-01-23', 1300, NULL, 10);
+CREATE TABLE IF NOT EXISTS dept (
+    deptno INT PRIMARY KEY,
+    dname VARCHAR(20),
+    loc VARCHAR(20)
+);
+INSERT INTO dept VALUES (10, 'ACCOUNTING', 'NEW YORK');
+INSERT INTO dept VALUES (20, 'RESEARCH', 'DALLAS');
+INSERT INTO dept VALUES (30, 'SALES', 'CHICAGO');
+INSERT INTO dept VALUES (40, 'OPERATIONS', 'BOSTON');
+-- 1. Fetch employee name, half salary, and location of employees working as MANAGER
+select e.ename, (e.sal/2), d.loc from emp e cross join dept d where e.job='manager';
+-- 2. Display all employee details along with department name and location
+select e.ename,e.empno,e.job,e.mgr,e.hiredate,e.sal,e.comm,e.deptno, d.dname, d.loc from emp e cross join dept d;
+-- 3. Display employee name, annual salary, joining date, and department information of all SALESMAN and ANALYST employees with alias names
+select e.ename,(e.sal*12) as annual_salary, e.hiredate, d.deptno, d.dname from emp e cross join dept d where job in ('sales','analyst'); 
