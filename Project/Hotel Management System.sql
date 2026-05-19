c7a38a932d8ea3e395742c14ab6e4fcd713d83cc
VaruneshT <tvarunesharasu@gmail.com>
2026-05-20 00:15:49 +0530

    Updated Hotel Management System.sql
    Created Rooms table

--- Project/Hotel Management System.sql
+++ Project/Hotel Management System.sql
@@ -8,3 +8,10 @@ CREATE TABLE Customers (
     phone VARCHAR(15) UNIQUE,
     city VARCHAR(50)
 );
+
+CREATE TABLE Rooms (
+    room_id INT PRIMARY KEY AUTO_INCREMENT,
+    room_type VARCHAR(30),
+    price_per_day DECIMAL(10,2) CHECK(price_per_day > 0),
+    room_status VARCHAR(20)
+);
