a01ea064419c8a2eaa017f297ef22ca9917d0703
VaruneshT <tvarunesharasu@gmail.com>
2026-05-20 00:15:06 +0530

    Updated Hotel Management System.sql
    Created Customers table

--- Project/Hotel Management System.sql
+++ Project/Hotel Management System.sql
@@ -1,2 +1,10 @@
 CREATE DATABASE hotel_management;
 USE hotel_management;
+
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50),
+    phone VARCHAR(15) UNIQUE,
+    city VARCHAR(50)
+);
