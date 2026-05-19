7465742cd9ef7a9efd92aa3f6d6408c31fcf1430
VaruneshT <tvarunesharasu@gmail.com>
2026-05-19 09:55:37 +0530

    Updated LimitBy_OffSet.sql
    Performed basic limiby and offset operations

--- LimitBy_OffSet.sql
+++ LimitBy_OffSet.sql
@@ -10,3 +10,17 @@ select distinct sal from emp order by sal desc limit 3 offset 2;
 select distinct sal from emp order by sal asc limit 3 offset 2;
 -- 4. Find the nth highest salary
 select * from emp limit 10 offset 8;
+
+
+
+-- Practice Question
+-- 1. Display first 5 employees
+select * from emp limit 5;
+-- 2. Display top 3 highest paid employees
+select * from emp order by sal desc limit 3;
+-- 3. Display lowest 2 salary employees
+select distinct sal from emp order by sal asc limit 2;
+-- 4. Display the 3rd highest distinct salary from the emp table using LIMIT and OFFSET
+select distinct sal from emp order by sal desc limit 1 offset 2;
+-- 5. Find the 5th Highest Distinct Salary
+select distinct sal from emp order by sal desc limit 1 offset 4;
